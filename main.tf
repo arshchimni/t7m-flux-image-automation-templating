65ce96e1225a7fcb1836aa0c020e37894ddb8fa1
a-chimni <a-chimni@mercari.com>
2021-08-05 13:32:30 +0530

    use locals remove template

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ data "template_file" "flux_image_policy" {
 
 data "template_file" "flux_image_registry" {
   template = templatefile("${path.module}/templates/podinfo-registry.tmpl", { namespace = var.namespace, images = var.user_images, interval = "1m" })
-  vars = {}
-  
+  vars     = {}
+
 }
 
 # data "local_file" "flux_image_registry" {
@@ -17,11 +17,13 @@ data "template_file" "flux_image_registry" {
 # }
 
 locals {
-  resources = split("\n---\n", data.template_file.flux_image_registry.rendered)
+
+  test      = templatefile("${path.module}/templates/podinfo-registry.tmpl", { namespace = var.namespace, images = var.user_images, interval = "1m" })
+  resources = split("\n---\n", local.test)
 }
 
 
-# resource "local_file" "image_repository_manifests" {
-#     content  = join(" ",local.resources)
-#     filename = "image_repository_rendered_example.txt"
-# }
+resource "local_file" "image_repository_manifests" {
+    content  = join(" ",local.resources)
+    filename = "image_repository_rendered_example.txt"
+}
--- output.tf
+++ output.tf
@@ -2,10 +2,10 @@ output "image_policy" {
   value = data.template_file.flux_image_policy.rendered
 }
 
-output "image_registry" {
-  value = data.template_file.flux_image_registry.rendered
-}
+# output "image_registry" {
+#   value = data.template_file.flux_image_registry.rendered
+# }
 
 output "image_registry_locals" {
-  value = local.resources
+  value = local.test
 }
